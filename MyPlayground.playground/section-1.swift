2855910d6ec36a05489090ebd9bb7255e5116ed3
Roy McFarland <roy.mcfarland@gmail.com>
2015-01-24 20:57:11 -0700

    Initial commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+///////////////////////////////////
+//////////// VARIABLES ////////////
+///////////////////////////////////
+
+// Implicit (Inferred)
+var str = "Hello "
+var modernProgrammingLanguage = "Swift"
+var greeting = str + modernProgrammingLanguage
+
+// Explicit
+// var str : String = "Hello "
+// var modernProgrammingLanguage : String = "Swift"
+// var greeting : String = str + modernProgrammingLanguage
+
+
+///////////////////////////////////
+/////////// CONSTANTS /////////////
+///////////////////////////////////
+
+// Implicit (Inferred)
+let theDude = "Jeffrey Lebowski"
+
+// Explicit
+// let theDude : String = "Jeffrey Lebowski"
